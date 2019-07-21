b480fe5c0e5f4c226879d5dd23d3296b6a33e331
igor <mako.mmw@gmail.com>
2019-07-21 20:44:44 +0300

    Исправлен путь до словаря

--- src/Alisa/Traits/Morphy.php
+++ src/Alisa/Traits/Morphy.php
@@ -21,7 +21,7 @@ Trait Morphy
      */
     public function convertToBaseForm(array $words): array
     {
-        $morphy = new phpMorphy(new phpMorphy_FilesBundle(__DIR__ . '/../../../data/dictionary/', 'rus'), [
+        $morphy = new phpMorphy(new phpMorphy_FilesBundle($_SERVER['DOCUMENT_ROOT'] .  '/../vendor/phpmorphy/dicts/', 'rus'), [
             'storage' => PHPMORPHY_STORAGE_FILE,
             'with_gramtab' => false,
             'predict_by_suffix' => true,

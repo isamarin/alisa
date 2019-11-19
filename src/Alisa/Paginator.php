47ec687c07b20d3c90645af7270a30fa809e5330
igor <mako.mmw@gmail.com>
2019-11-19 18:13:22 +0300

    Дополнена работа пагинатора

--- src/Alisa/Paginator.php
+++ src/Alisa/Paginator.php
@@ -3,45 +3,32 @@
 namespace isamarin\Alisa;
 
 use function array_key_exists;
+use function array_slice;
 use function count;
 
+/**
+ * Class Paginator
+ * @package isamarin\Alisa
+ */
 class Paginator
 {
     protected $links;
     protected $buttons;
     protected $payload;
 
-    protected $limit = 3;
+    protected $limit = 5;
 
     protected $topage;
     protected $current;
     protected $prevTrigger;
 
-    //TODO
-    //[services][back]
-    //[services][current]
-    /**
-     * Paginator constructor.
-     * @param Button ...$buttons
-     * TODO
-     * how to match?
-     */
     public function __construct($requestPayload, $prevTrigger)
     {
         $this->payload = $requestPayload;
         $this->prevTrigger = $prevTrigger;
-        writeLog($this->payload);
         if (array_key_exists('services', $this->payload)) {
-            $this->current = $this->payload['services']['current'];
             $this->topage = $this->payload['services']['topage'];
         }
-//        foreach ($buttons as $button) {
-//            if (array_key_exists('link', $button->get())) {
-//                $this->links[] = $button;
-//            } else {
-//                $this->buttons[] = $button;
-//            }
-//        }
     }
 
     public function append(Button $button)
@@ -53,10 +40,6 @@ class Paginator
         }
     }
 
-    /**
-     * TODO
-     * add service
-     */
     public function getPaginated()
     {
         $this->addServiceButtons();
@@ -65,46 +48,38 @@ class Paginator
 
     protected function addServiceButtons()
     {
-        if (count($this->links) > $this->limit) {
+        $currentCount = count($this->links);
 
-            if ( ! isset($this->topage, $this->current)) {
-                $this->topage = 2;
-                $this->current = 1;
+        if ($currentCount > $this->limit) {
+
+
+            if ( ! isset($this->topage)) {
+                $this->topage = 1;
             }
-            writeLog('PREV: ' . $this->prevTrigger->getName());
 
-            $this->links = array_slice($this->links, ($this->current - 1) * $this->limit, $this->limit);
-            $back = new Button();
-            $back->setTitle('Назад');
-            $back->linkTrigger($this->prevTrigger);
-            $back->setHide(false);
-            $back->addPayload([
-                'topage' => $this->topage - 1,
-                'current' => $this->topage,
-            ]);
-
-            if ($this->current !== 1) {
+            $this->links = array_slice($this->links, ($this->topage - 1) * $this->limit, $this->limit);
+            $less = $currentCount - ($this->topage * $this->limit);
+            if ($less !== 0 && $this->topage !== 1) {
+                $back = new Button();
+                $back->setTitle('Назад');
+                $back->linkTrigger($this->prevTrigger);
+                $back->setHide(false);
+                $back->addPayload([
+                    'topage' => $this->topage - 1,
+                ]);
                 $this->links[] = $back;
             }
+            if ($less > 0) {
+                $more = new Button();
+                $more->setTitle("Ещё ($less)");
+                $more->linkTrigger($this->prevTrigger);
+                $more->setHide(false);
+                $more->addPayload([
+                    'topage' => $this->topage + 1,
+                ]);
+                $this->links[] = $more;
+            }
 
-            /**
-             * TODO
-             * count MORE
-             */
-
-            /**
-             * TODO
-             * hide more
-             */
-            $more = new Button();
-            $more->setTitle('Еще');
-            $more->linkTrigger($this->prevTrigger);
-            $more->setHide(false);
-            $more->addPayload([
-                'topage' => $this->topage + 1,
-                'current' => $this->topage,
-            ]);
-            $this->links[] = $more;
         }
     }
 }
